310815aa85c61b4d08eb93de0cdbf3fa558706ff
alievaleila <meliyeva452@gmail.com>
2024-07-02 16:12:56 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,80 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Car
+        String color = "blue";
+        int numberOfwheels = 4;
+        double weight = 1250.5;
+        boolean hasDriver = true;
+        byte numberOfdoors = 4;
+        long milage = 24387228;
+        float engineVolume = 2.5f;
+        //Book
+        String author="Agatha Christie";
+        int numberOfpage=700;
+        double width=5.5;
+        boolean coverOfbook=true;
+        byte numberOfauthor=1;
+        long numbersofreader=2000050237;
+        float height=20.6f;
+        //Student
+        String name="Leyla";
+        int age=20;
+        double weightOfstudent=(double)51.3;
+        boolean legthOfhair=true;
+        byte numbersOfpen=3;
+        long heightInMm=1632;
+        float heightofStudent=163.2f;
+        //Table
+        String colorOftable="brown";
+        int numbersOfchair=4;
+        double heightOfchairsInM=1.35;
+        boolean Vase=false;
+        byte widthofcircleInM=2;
+        long heightoflegsinMM=1383;
+        float heightofTableInM=6.5f;
+        //Tv
+        String brand="Samsung";
+        int price =1800;
+        double widthOfmonitor=3.3;
+        boolean accestoInternet=true;
+        byte numbersofdigitalCamera =1;
+        long heightinMm =1700;
+        float lengthofdiogonal=1.8f;
+        //door
+        String material="wooden";
+        int weightOfdoorInkq=34;
+        double widthofDoor = 1.7;
+        boolean haspeephole=true;
+        byte numbersofkeys=3;
+        long heightofdoorinMm=2465;
+        float lengthofKey=4.7f;
+        //curtain
+        String colorOfcurtain="pink";
+        int lengthOfcurtain =3;
+        double widthofcurtain = 6.5;
+        boolean decorated=true;
+        byte numbersofDecoration=7;
+        long lengthofcurtaininMm=3637;
+        float  diogonalofcurtaininM=4.2f;
+        //phone
+        String brandofphone="Xiomi";
+        int diogonalofphoneinCm=14;
+        double widthOfphone= 6.3;
+        boolean hasPhoneCase=true;
+        byte numbersofcamera=3;
+        long memory_capacityInMb=128000;
+        float diametreoffrontCamera=0.4f;
+
+
+
+
+
+
+
+
+
+
+    }
+}
